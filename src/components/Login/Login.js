cdd4559aa53e4d67c404ef1cc9c85c2d8752a4c6
n00bNn00b <reldewan9@gmail.com>
2022-04-16 16:58:26 +0600

    customHook for google sign in and added form for register and login page

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,9 +1,23 @@
 import React from "react";
+import useFirebase from "../../hooks/useFirebase";
+import "./Login.css";
 
 const Login = () => {
+  const { signInWithGoogle } = useFirebase();
   return (
     <div>
       <h2>Login Page</h2>
+      <div className="login-btn">
+        <button onClick={signInWithGoogle}>Google Sign In</button>
+      </div>
+
+      <form>
+        <input type="email" name="" placeholder="Your Email" />
+        <br />
+        <input type="password" name="" id="" placeholder="password" />
+        <br />
+        <input className="login-btn" type="submit" value="Login" />
+      </form>
     </div>
   );
 };
---
+++ src/components/Register/Register.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Register = () => {
+  return (
+    <div>
+      <h2>Register Here</h2>
+      <form>
+        <input type="text" name="" placeholder="Your Name" />
+        <br />
+        <input type="email" name="" placeholder="Your Email" />
+        <br />
+        <input type="password" name="" id="" placeholder="password" />
+        <br />
+        <input type="submit" value="Resgiter" />
+      </form>
+    </div>
+  );
+};
+
+export default Register;
